1acb69f1baaaef3a5ae5c5e90338ffaf3caf7f40
Thahaseen <mthahaseen@gmail.com>
2016-11-03 11:26:20 +0530

    Initial Commit

---
+++ app/src/main/java/amgen/com/jenkinstest/model/User.java
@@ -0,0 +1,16 @@
+package amgen.com.jenkinstest.model;
+
+/**
+ * Created by 538280 on 10/26/2016.
+ */
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
---
+++ app/src/test/java/amgen/com/jenkinstest/model/UserTest.java
@@ -0,0 +1,23 @@
+package amgen.com.jenkinstest.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import amgen.com.jenkinstest.BuildConfig;
+import amgen.com.jenkinstest.MainActivity;
+
+
+/**
+ * Created by 538280 on 10/26/2016.
+ */
+
+public class UserTest {
+
+    @Test
+    public void UserNameTest() {
+        User u = new User();
+        u.setName("foo");
+        Assert.assertEquals("foo", u.getName());
+    }
+
+}

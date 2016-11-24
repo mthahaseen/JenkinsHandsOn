7b4bbdf4d686e1d06eb79a568d76df35c998070d
Thahaseen <mthahaseen@gmail.com>
2016-11-24 17:34:19 +0530

    espresso test cases

---
+++ app/src/androidTest/java/amgen/com/jenkinstest/MainActivityTest.java
@@ -0,0 +1,32 @@
+package amgen.com.jenkinstest;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureTextChangesWork() {
+        onView(withId(R.id.inputField))
+                .perform(typeText("HELLO"), closeSoftKeyboard());
+        onView(withId(R.id.changeText)).perform(click());
+        onView(withId(R.id.inputField)).check(matches(withText("HelloWorld!")));
+    }
+
+}
--- app/src/main/java/amgen/com/jenkinstest/MainActivity.java
+++ app/src/main/java/amgen/com/jenkinstest/MainActivity.java
@@ -2,6 +2,8 @@ package amgen.com.jenkinstest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = (EditText) findViewById(R.id.inputField);
+        findViewById(R.id.changeText).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText("HelloWorld!");
+            }
+        });
     }
 }
